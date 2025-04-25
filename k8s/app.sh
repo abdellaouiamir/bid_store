61d61bc7ff20f1469c0c160070350b6c1894cb45
abdellaouiamir <abdellaouiamir27@gmail.com>
2025-04-25 10:32:08 +0100

    finally

--- k8s/app.sh
+++ k8s/app.sh
@@ -1,16 +1,20 @@
-#!/usr/bin/env bash
-
 kubectl apply -f ./config-server-k8s.yml
-sleep 1
+sleep 20
 kubectl apply -f ./client-k8s.yml
 sleep 1
 kubectl apply -f ./product-k8s.yml
 sleep 1
+kubectl apply -f ./notification-k8s.yml
+sleep 20
 kubectl apply -f ./bid-k8s.yml
 sleep 1
 kubectl apply -f ./payment-k8s.yml
-sleep 1
-kubectl apply -f ./notification-k8s.yml
-sleep 1
+sleep 10
 kubectl apply -f ./env/nginx-k8s.yml
-sleep 1
+sleep 5
+check () {
+  echo $(kubectl get pods | grep Running | wc -l)
+}
+while [[ $(check) -ne 14 ]]; do
+  sleep 5
+done
--- k8s/env.sh
+++ k8s/env.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
-
+kubectl apply -f ./env/zookeeper-k8s.yaml
+sleep 1
 kubectl apply -f ./env/mongo-k8s.yaml
 sleep 1
 kubectl apply -f ./env/postgres-k8s.yaml
@@ -9,6 +9,12 @@ sleep 1
 kubectl apply -f ./env/zipkin-k8s.yaml
 sleep 1
 kubectl apply -f ./env/node-mailer-k8s.yaml
-sleep 1
-#kubectl apply -f ./notification-k8s.yml
-sleep 1
+sleep 5
+check () {
+  echo $(kubectl get pods | grep Running | wc -l)
+}
+while [[ $(check) -ne 6 ]]; do
+  sleep 5
+done
+kubectl apply -f ./env/kafka-k8s.yaml
+sleep 5
